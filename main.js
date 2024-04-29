9856d81547c21ba4ddc497eb8039cae5c11226b6
DmitryR <dmytro.ryabchun@gmail.com>
2024-04-29 20:01:45 +0300

    ✨ Added title bar

--- main.js
+++ main.js
@@ -6,19 +6,18 @@ function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        titleBarStyle: 'hidden',
+        icon: 'assets/images/icon.png',
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     });
 
-    ipcMain.on('get-file-data', function(event) {
-        let data = null;
-        if (process.platform == 'win32' && process.argv.length >= 2) {
-            let openFilePath = process.argv[1]
-            data = openFilePath
-        }
-        event.returnValue = data
-    })
+    watchGetFileData();
+    watchQuitApp();
+    watchCollapseWindow(win);
+    watchMaximizeWindow(win);
+    watchRestoreWindow(win);
 
     win.loadFile('index.html').then();
 }
@@ -37,4 +36,39 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
+
+function watchGetFileData() {
+    ipcMain.on('get-file-data', function(event) {
+        let data = null;
+        if (process.platform == 'win32' && process.argv.length >= 2) {
+            let openFilePath = process.argv[1];
+            data = openFilePath;
+        }
+        event.returnValue = data;
+    });
+}
+
+function watchQuitApp() {
+    ipcMain.handle('quit-app', () => {
+        app.quit();
+    });
+}
+
+function watchCollapseWindow(window) {
+    ipcMain.handle('collapse-app', () => {
+        window.minimize();
+    });
+}
+
+function watchMaximizeWindow(window) {
+    ipcMain.handle('maximize-window', () => {
+        window.maximize();
+    });
+}
+
+function watchRestoreWindow(window) {
+    ipcMain.handle('restore-window', () => {
+        window.restore();
+    });
+}
